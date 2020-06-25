541e470e362b80fa0b335e40855c67c9cd027ab9
Cody Ilardo <cody@perryman.local>
2020-06-25 15:17:01 -0400

    refactored popup.js to try deocdeURIComponent and updated version

--- popup.js
+++ popup.js
@@ -1,92 +1,84 @@
 function loadResponse(query) {
 
-  $.ajax( {
-    type: "GET",
-    dataType: "json",
-    headers: {
-      "Authorization": "Token 8031526c5738ab9ca0de8b9dc7e6a363ba36b84e"
-    },
-    url: "https://api.esv.org/v3/passage/search/",
-    data: {
-      "q": query,
-      "page-size":100
-    },
-    // remove this success one? done() handles it
-    success: function(data) {
-      console.log("success");
-    },
-    error: function() {
-      console.log("failure");
-    }
-  }).done(function(data) {
-    console.log(JSON.stringify(data));
-    createHTML(query, data);
-  });
+    $.ajax( {
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "Authorization": "Token 8031526c5738ab9ca0de8b9dc7e6a363ba36b84e"
+        },
+        url: "https://api.esv.org/v3/passage/search/",
+        data: {
+            "q": query,
+            "page-size":100
+        },
+        error: function() {
+            console.log("failure");
+        }
+    }).done(function(data) {
+        console.log(JSON.stringify(data));
+        createHTML(query, data);
+    });
 }
 
 function createHTML(query,json) {
 
-  var num_results = document.createElement("h2");
-  num_results.classList.add("success");
-  num_results.classList.add("text-success");
-  num_results.innerHTML = json["total_results"] + " results for " + '"' + query + '"';
-  jumbo.append(num_results);
+    var num_results = document.createElement("h2");
+    num_results.classList.add("success");
+    num_results.classList.add("text-success");
+    num_results.innerHTML = json["total_results"] + " results for " + '"' + query + '"';
+    jumbo.append(num_results);
 
-  var output = document.getElementById('output_stream');
+    var output = document.getElementById('output_stream');
 
-  // if too many results
-  if (json["total_pages"]>1) {
-    var redir_link = document.createElement("p");
-    redir_link.classList.add("redir_err");
-    redir_link.classList.add("text-danger");
-    redir_link.innerHTML = "Too many results for query. Click <a href=https://www.google.com/search?q=" + query + "+in+the+bible target='_blank'>here</a> for more."
-    output.append(redir_link);
+    // if too many results
+    if (json["total_pages"]>1) {
+        var redir_link = document.createElement("p");
+        redir_link.classList.add("redir_err");
+        redir_link.classList.add("text-danger");
+        redir_link.innerHTML = "Too many results for query. Click <a href=https://www.google.com/search?q=" + query + "+in+the+bible target='_blank'>here</a> for more."
+        output.append(redir_link);
 
-  } else {
+    } else {
 
-    var json_data = json["results"];
-    for (var i = 0; i < json_data.length; i++) {
+        var json_data = json["results"];
+        for (var i = 0; i < json_data.length; i++) {
+            var obj = json_data[i];
+            var obj_ref = document.createElement("h3");
+            obj_ref.innerHTML = "<a href='https://www.esv.org/" + obj["reference"] + "' target='_blank'>"+obj["reference"]+"</a>";
+            var obj_con = document.createElement("p");
+            obj_con.innerHTML = obj["content"];
 
-      var obj = json_data[i];
-      var obj_ref = document.createElement("h3");
-      obj_ref.innerHTML = "<a href='https://www.esv.org/" + obj["reference"] + "' target='_blank'>"+obj["reference"]+"</a>";
-      var obj_con = document.createElement("p");
-      obj_con.innerHTML = obj["content"];
+            console.log(obj["reference"]);
+            console.log(obj["content"]);
 
-      console.log(obj["reference"]);
-      console.log(obj["content"]);
-
-      output.append(obj_ref);
-      output.append(obj_con);
-      }
+            output.append(obj_ref);
+            output.append(obj_con);
+        }
     }
 }
 
+// browser action in manifest.json tells this to run on click
 chrome.tabs.executeScript( {
-  code: "window.getSelection().toString();"
-}, function(selection) {
-  var query_raw_raw = encodeURIComponent(selection[0] || '汉典');
-  var jumbo = document.getElementById('jumbo');
-  console.log(selection);
-  if (selection[0]=="") {
-    var no_selection = document.createElement("h2");
-    no_selection.classList.add("text-danger");
-    no_selection.classList.add("notext-err");
-    no_selection.innerHTML = "No text selected";
-    jumbo.append(no_selection);
-
-  } else {
-
-    // next three lines not needed? possibly
-    var query_raw = query_raw_raw.replace(/%20/g, " ");
-    query_raw.replace(/%22/g, " ");
-    query_raw.replace(/%2F/g, "/");
-    var query = decodeURIComponent(query_raw.trim())
-
-    loadResponse(query);
-    // have load response resolve to a variable
-    // render the variable here with create html, rather than having loadResponse call createHTML
-  }
-
+    code: "window.getSelection().toString();"},
+    function(selection) {
+        var query_raw_raw = encodeURIComponent(selection[0] || '汉典');
+        var jumbo = document.getElementById('jumbo');
+        console.log(selection);
+        if (selection[0]=="") {
+            var no_selection = document.createElement("h2");
+            no_selection.classList.add("text-danger");
+            no_selection.classList.add("notext-err");
+            no_selection.innerHTML = "No text selected";
+            jumbo.append(no_selection);
+        } else {
+            var query_raw = query_raw_raw.replace(/\+/g, ' ');
+            try {
+                var query = decodeURIComponent(query_raw.trim());
+            } catch(e) {
+                console.error(e);
+                query = query_raw;
+            }
+        }
+        loadResponse(query);
 }
 );
